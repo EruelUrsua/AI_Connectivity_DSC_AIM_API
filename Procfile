0e13fe2839ec4dbcc315fb5b694e874847350f04
EruelUrsua <eruelursua@gmail.com>
2025-01-26 23:14:41 +0800

    finals

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn risk_factor_outage_API:app --bind 0.0.0.0:$PORT
--- api/risk_factor_outage_API.py
+++ api/risk_factor_outage_API.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model_loader import load_model
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -70,4 +71,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
